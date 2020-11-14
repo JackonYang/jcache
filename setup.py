ad7697de6334f85a4abeedb6dd19a62eba2b011d
Jackon Yang <i@jackon.me>
2020-11-14 12:23:18 +0800

    v0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='jcache',
-      version='0.2',
+      version='0.3',
       description=('a library that can cache the return of a function '
                    'to avoid repeated computing'),
       url='https://github.com/JackonYang/jcache',
